64bf3a194b92a51e9896bee5fecb0787bf4264dc
MyUsername <rosanio.c@gmail.com>
2024-10-21 17:23:51 +0200

    ESERCIZI 1 + 2

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+// Dichiaro un nuovo array vuoto dove accumulerò il risultato finale
+const firstCharacters = [];
+
 
 // Dichiara la funzione qui.
-
+names.forEach(function(currentItem) {
+    let firstCharacter = currentItem.charAt(0);
+    firstCharacters.push(firstCharacter);
+})
 
 // Invoca la funzione qui e stampa il risultato in console
 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+console.log(firstCharacters);
--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -2,12 +2,30 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+const searchedSubstring = 'A';
+
 
 // Dichiara la funzione qui.
+const filterBySubstring = (needle, haystack) => {
 
+    filteredArray = [];
+    
+    for (let i = 0; i < haystack.length; i++) {
+        currentItem = haystack[i];
+        console.log('currentItem: ' + currentItem);
 
+    
+        if (needle === currentItem.charAt(0)) {
+            filteredArray.push(currentItem);
+        }
+    }
+
+    return filteredArray;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
+let myResult = filterBySubstring(searchedSubstring, names);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
+console.log(myResult);
