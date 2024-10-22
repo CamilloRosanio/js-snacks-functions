4589a067aaf3845a11acbc6e305ac340e5bab11a
MyUsername <rosanio.c@gmail.com>
2024-10-22 09:39:33 +0200

    CORREZIONE esercizio 3 Conteggio Vocali

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,38 +1,39 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
-// Somma del conteggio delle vocali in una parola
-let sum = 0;
-
-// Funzione che determina se un carattere siau na vocale o no
-let isVowel = 0;
-
-// Array dove finiranno le vocali conteggiate
-const vowelsInString = [];
-
-
-// Dichiaro una funzione semplice per confrontare un carattere e verificare se sia una vocale.
-const isVowel = (stringToCheck) => {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-    if(vowels.includes(stringToCheck)) {
-        return true;
-    }
-}
+const word = prompt('Inserisci una parola o una frase');
 
 
 // Dichiara la funzione qui.
-word.forEach((currentCharacter) => {
+
+/**
+ * La funzione conta le vocali in una stringa, aggiungendole a un nuovo Array e indicando come risultato la lunghezza dell'Array.x
+ * @param {String} string String è il parametro esterno di tipo stringa da elaborare
+ * @return {Array} validCharacters è l'array reso della funzione, e la sua lunghezza è il numero di vocali contenute nella stringa.
+ */
+function vowelsCount(string) {
+    const vowelsList = ['a','e','i','o','u','A','E','I','O','U'];
+    const vowelsCount = [];
+    let currentCharacter;
+    let stringArray = string.split("");
+
+    console.log('Stringa da elaborare: ' + stringArray);
+
+    for (let i = 0; i < stringArray.length; i++) {
+        currentCharacter = stringArray[i];
     
+        if (vowelsList.includes(currentCharacter)) {
+            vowelsCount.push(currentCharacter);
+        }
+        
+        console.log('Current character: "' + currentCharacter + '" is included: ' + vowelsList.includes(currentCharacter));
+    }
+
+    return vowelsCount;
 }
 
 
-)
-
-
 // Invoca la funzione qui e stampa il risultato in console
-
-console.log(vowelsFound);
-
+let myResult = vowelsCount(word);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+console.log(myResult);
